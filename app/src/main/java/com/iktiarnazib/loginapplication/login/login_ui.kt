bf19ac9d2bb7b62904febb4a087516668957851d
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-25 22:55:10 +0600

    Premier University Logo added

--- app/src/main/java/com/iktiarnazib/loginapplication/login/login_ui.kt
+++ app/src/main/java/com/iktiarnazib/loginapplication/login/login_ui.kt
@@ -1,12 +1,15 @@
 package com.iktiarnazib.loginapplication.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -18,10 +21,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.w3c.dom.Text
+import com.iktiarnazib.loginapplication.R
 
 @Preview(showBackground = true, showSystemUi = true
 )
@@ -30,57 +35,75 @@ fun LoginScreen(modifier: Modifier = Modifier) {
     var email = remember { mutableStateOf("") }
     var password = remember { mutableStateOf("") }
     var errorMessage = remember { mutableStateOf("") }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Welcome back", style = MaterialTheme.typography.headlineMedium)
-        Spacer(modifier = Modifier.height(14.dp))
-        OutlinedTextField(
-            value = email.value,
-            onValueChange = {
-                email.value = it
-            },
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth(),
-            label = {Text(text = "Email")},
-            shape = RoundedCornerShape(20.dp)
-        )
+    Box(
+modifier = Modifier.fillMaxSize(),
 
-        OutlinedTextField(
-            value = password.value,
-            onValueChange = {
-                password.value = it
-            },
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth(),
-            label = {Text(text = "Password")},
-shape = RoundedCornerShape(20.dp)
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.login_bg),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+            , alpha = 0.4f
 
         )
-
-
-        if(errorMessage.value.isNotEmpty()){
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = errorMessage.value, color = Color.Red)
-        }
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Button(
-            onClick = {
-                if(email.value.isNotBlank() && password.value.isNotBlank()){
-                    errorMessage.value = ""
-                } else {
-                    errorMessage.value = "Please fill both textfields"
-                }
-            },
-            modifier = Modifier.fillMaxWidth()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Login")
+            Image(
+                painter = painterResource(id = R.drawable.puclogo),
+                contentDescription = "Puc logo",
+                modifier = Modifier.size(120.dp))
+
+            Text(text = "Welcome back", style = MaterialTheme.typography.headlineLarge)
+            Spacer(modifier = Modifier.height(14.dp))
+            OutlinedTextField(
+                value = email.value,
+                onValueChange = {
+                    email.value = it
+                },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(),
+                label = {Text(text = "Email")},
+                shape = RoundedCornerShape(20.dp)
+            )
+
+            OutlinedTextField(
+                value = password.value,
+                onValueChange = {
+                    password.value = it
+                },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(),
+                label = {Text(text = "Password")},
+                shape = RoundedCornerShape(20.dp)
+
+            )
+
+
+            if(errorMessage.value.isNotEmpty()){
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = errorMessage.value, color = Color.Red)
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = {
+                    if(email.value.isNotBlank() && password.value.isNotBlank()){
+                        errorMessage.value = ""
+                    } else {
+                        errorMessage.value = "Please fill both textfields"
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Login")
+            }
         }
     }
 }
