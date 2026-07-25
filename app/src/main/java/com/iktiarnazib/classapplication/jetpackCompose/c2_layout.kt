b4dd694bbea891cb06eaeba2c3b9edc6a826ca64
Iktiar Nazib <iktiarnazib@gmail.com>
2026-07-25 21:39:50 +0600

    Login screen kotlin

--- app/src/main/java/com/iktiarnazib/classapplication/jetpackCompose/c2_layout.kt
+++ app/src/main/java/com/iktiarnazib/classapplication/jetpackCompose/c2_layout.kt
@@ -5,12 +5,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import org.w3c.dom.Text
 
 @Composable
 private fun RowExample() {
@@ -25,10 +28,8 @@ private fun RowExample() {
 }
 
 //column
-@Preview(showBackground = true, showSystemUi = true)
-@Preview
 @Composable
-private fun columnExample() {
+private fun ColumnExample() {
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -39,4 +40,33 @@ private fun columnExample() {
         Text(text = "Banana",  fontSize = 30.sp);
         Text(text = "Mango",  fontSize = 30.sp);
     }
+}
+
+
+//column
+@Preview(showBackground = true, showSystemUi = true)
+@Preview
+@Composable
+private fun ColumnLoginExample() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Login Here", fontSize = 30.sp);
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "Enter your name") }
+        )
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Enter your password") }
+        )
+    }
+
 }
